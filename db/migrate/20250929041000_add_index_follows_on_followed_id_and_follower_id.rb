d86fd7326d65c2924385a781deb027e243ea7087
Arya Surya <suryapradanaput@gmail.com>
2025-09-29 12:08:17 +0700

    refactor: correct syntax for adding index on followed_id and follower_id in follows table

--- db/migrate/20250929041000_add_index_follows_on_followed_id_and_follower_id.rb
+++ db/migrate/20250929041000_add_index_follows_on_followed_id_and_follower_id.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
+# Migration to add an index on followed_id and follower_id in follows table
 class AddIndexFollowsOnFollowedIdAndFollowerId < ActiveRecord::Migration[7.1]
   def change
-    add_index :follows, [:followed_id, :follower_id], name: 'index_follows_on_followed_id_and_follower_id'
+    add_index :follows, %i[followed_id follower_id], name: 'index_follows_on_followed_id_and_follower_id'
   end
 end
